2e1ec8c6aba149608ff9eb7624aaa11aeaac2781
Nadeem Mohammad <nadeembtech@gmail.com>
2021-06-04 09:07:29 +0530

    refactored

--- src/main/java/com/org/lob/config/RepositoryConfig.java
+++ src/main/java/com/org/lob/config/RepositoryConfig.java
@@ -4,18 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.envers.repository.config.EnableEnversRepositories;
-import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.org.lob.support.SpringSecurityAuditorAware;
 
 @Configuration
-@EnableEnversRepositories
-@EnableTransactionManagement
-@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class, basePackages = {"com.org.lob.project.repository"}) 
 @EnableJpaAuditing
+@EnableEnversRepositories(basePackages = {"com.org.lob.project.repository"})
+@EnableTransactionManagement
 public class RepositoryConfig {
 
 	@Bean
--- src/main/java/com/org/lob/project/repository/entity/Customer.java
+++ src/main/java/com/org/lob/project/repository/entity/Customer.java
@@ -1,14 +1,12 @@
 package com.org.lob.project.repository.entity;
 
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,20 +15,16 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 
+import org.hibernate.envers.AuditOverride;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.NotAudited;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity(name = "PRJ_CUSTOMER")
 @Audited
-@EntityListeners(AuditingEntityListener.class)
-public class Customer implements Serializable {
+@AuditOverride(forClass = Auditable.class)
+public class Customer extends Auditable implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -54,22 +48,6 @@ public class Customer implements Serializable {
     @JsonManagedReference
     @NotAudited
     private List<Address> addresses;
-
-    @CreatedBy
-    @Column(name = "CREATED_BY", columnDefinition = "varchar(25)", updatable = false)
-    private String createdBy;
-
-    @CreatedDate
-    @Column(name = "CREATE_DATE_TIME", columnDefinition = "timestamp default '2021-06-10 20:47:05.967394'", updatable = false)
-    private LocalDateTime createdDate;
-
-    @LastModifiedBy
-    @Column(name = "MODIFIED_BY", columnDefinition = "varchar(25)")
-    private String lastModifiedBy;
-
-    @LastModifiedDate
-    @Column(name = "MODIFIED_DATE_TIME", columnDefinition = "timestamp default '2021-06-10 20:47:05.967394'")
-    private LocalDateTime lastModifiedDate;
 
 	public Long getId() {
 		return id;
@@ -115,38 +93,6 @@ public class Customer implements Serializable {
 		}
 		this.addresses.addAll(addresses);
 	}
-	
-	public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public String getLastModifiedBy() {
-        return lastModifiedBy;
-    }
-
-    public void setLastModifiedBy(String lastModifiedBy) {
-        this.lastModifiedBy = lastModifiedBy;
-    }
-
-    public LocalDateTime getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
 
 	@Override
 	public String toString() {
