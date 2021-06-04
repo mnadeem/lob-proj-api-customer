092209253bb06ac01d513dc6f8fa1cde22399092
Nadeem Mohammad <nadeembtech@gmail.com>
2021-06-04 10:56:40 +0530

    batch

---
+++ src/main/java/com/org/lob/config/BatchConfig.java
@@ -0,0 +1,18 @@
+package com.org.lob.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+}
