754d6eed0d547c5c20b272c9f89c374ac154a5d5
Nadeem Mohammad <nadeembtech@gmail.com>
2021-06-05 12:51:26 +0530

    cONSTANTS

---
+++ src/main/java/com/org/lob/support/Constants.java
@@ -0,0 +1,17 @@
+package com.org.lob.support;
+
+public final class Constants {
+	
+	public static final String BATCH_JOB_ID = "jobID";
+	public static final String BATCH_JOB_FILE_NAME = "fileName";
+	public static final String BATCH_JOB_LAUNCH_DATE = "launchDate";
+	public static final String BATCH_JOB_ROOT_ELEMENT = "customer";
+	public static final String BATCH_JOB_TARGET_METHOD = "create";
+
+	public static final String SYSTEM_USER_DEFAULT = "SYSTEM";
+	
+	private Constants() {
+
+	}
+
+}
